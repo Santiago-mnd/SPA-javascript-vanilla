d51ec98d556944317551bd623a8fb004649b8932
santiago-mnd <santi_ga1020@hotmail.com>
2021-11-12 16:53:28 -0600

    Update bundle.js

--- src/template/Header.js
+++ src/template/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="header-main">
             <div class="header-logo">
                 <h1>
-                    <a href="/">
+                    <a href="/SPA-javascript-vanilla">
                         100tifi.co
                     </a>
                 </h1>
