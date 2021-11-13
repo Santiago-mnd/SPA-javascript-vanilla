4e65a6c92fcb7e44ffcb8cffc461bfbb39159b01
santiago-mnd <santi_ga1020@hotmail.com>
2021-11-12 22:47:47 -0600

    Add responsive

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,9 @@
 const Error404 = () => {
     const view = `
-        <div class="Error404">
+        <div class="error404">
             <h2>Error 404</h2>
+            <span><img src="https://pngimg.com/uploads/rick_morty/rick_morty_PNG13.png" aria-hidden=true></span>
+            <p>No me lo vas a creer...</p>
         </div>
     `
     return view
--- src/template/Header.js
+++ src/template/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="header-main">
             <div class="header-logo">
                 <h1>
-                    <a href="/SPA-javascript-vanilla">
+                    <a href="/SPA-javascript-vanilla/">
                         100tifi.co
                     </a>
                 </h1>
